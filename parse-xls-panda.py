778ef7537d980e6f279f456b52e7814d79e8f43b
jeremypruitt <jeremy@learnsecurely.com>
2019-04-14 16:26:43 -0700

    Initial workbook output

--- parse-xls-panda.py
+++ parse-xls-panda.py
@@ -3,6 +3,8 @@ from pandas import ExcelWriter
 from pandas import ExcelFile
 import json
 import math
+import xlwt
+import collections
 
 fileName = '03-06-2019_QL_Equip_List.xlsx'
 sheetName = 'Eq List'
@@ -94,3 +96,65 @@ for row_index, row in eqList.iterrows():
           found["sections"]["Miscellaneous_LIST"].append(section)
 
 print(json.dumps(found))
+
+reportFileName = "test-report-output.xls"
+sheetName = "SummaryReport"
+
+
+# JOB: client
+# JOB: poBox
+# JOB: jobNumber
+# JOB: sections
+def outputExcelReport(filename, sheet, data):
+  book = xlwt.Workbook()
+  sh = book.add_sheet(sheet)
+  
+  i = 0
+  sh.write(i,0,data["client"])
+  sh.write(i,1,data["jobNumber"])
+
+  for section in data["sections"]:
+    i += 1
+    print("section: " + str(section))
+    # if isinstance(data[key], collections.Mapping):
+    #   pass
+    # else:
+    #   sh.write(i,0,key)
+    #   sh.write(i,1,data[key])
+
+  book.save(filename)
+
+outputExcelReport(reportFileName, sheetName, found)
+
+
+def output(filename, sheet, list1, list2, x, y, z):
+    book = xlwt.Workbook()
+    sh = book.add_sheet(sheet)
+
+    variables = [x, y, z]
+    x_desc = 'Display'
+    y_desc = 'Dominance'
+    z_desc = 'Test'
+    desc = [x_desc, y_desc, z_desc]
+
+    col1_name = 'Stimulus Time'
+    col2_name = 'Reaction Time'
+
+    #You may need to group the variables together
+    #for n, (v_desc, v) in enumerate(zip(desc, variables)):
+    for n, v_desc, v in enumerate(zip(desc, variables)):
+        sh.write(n, 0, v_desc)
+        sh.write(n, 1, v)
+
+    n+=1
+
+    sh.write(n, 0, col1_name)
+    sh.write(n, 1, col2_name)
+
+    for m, e1 in enumerate(list1, n+1):
+        sh.write(m, 0, e1)
+
+    for m, e2 in enumerate(list2, n+1):
+        sh.write(m, 1, e2)
+
+    book.save(filename)
